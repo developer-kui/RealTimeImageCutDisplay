17bcca0d81e016f77e52cae4203dcb6927c68587
kui <kui.pirukuru@gmail.com>
2019-12-22 03:07:17 +0900

    commit

---
+++ RealTimeImageCutDisplay/RealTimeImageCutDisplay/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace RealTimeImageCutDisplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxDirectory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.updateTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(304, 344);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxDirectory
+            // 
+            this.textBoxDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDirectory.Enabled = false;
+            this.textBoxDirectory.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxDirectory.Location = new System.Drawing.Point(219, 3);
+            this.textBoxDirectory.Name = "textBoxDirectory";
+            this.textBoxDirectory.Size = new System.Drawing.Size(78, 25);
+            this.textBoxDirectory.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(0, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "y";
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxHeight.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBoxHeight.Location = new System.Drawing.Point(107, 31);
+            this.textBoxHeight.MaxLength = 4;
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(40, 25);
+            this.textBoxHeight.TabIndex = 4;
+            this.textBoxHeight.Text = "520";
+            this.textBoxHeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxY.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBoxY.Location = new System.Drawing.Point(18, 31);
+            this.textBoxY.MaxLength = 4;
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(40, 25);
+            this.textBoxY.TabIndex = 2;
+            this.textBoxY.Text = "85";
+            this.textBoxY.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxX.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBoxX.Location = new System.Drawing.Point(18, 2);
+            this.textBoxX.MaxLength = 4;
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(40, 25);
+            this.textBoxX.TabIndex = 1;
+            this.textBoxX.Text = "800";
+            this.textBoxX.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxWidth.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBoxWidth.Location = new System.Drawing.Point(107, 2);
+            this.textBoxWidth.MaxLength = 4;
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(40, 25);
+            this.textBoxWidth.TabIndex = 3;
+            this.textBoxWidth.Text = "270";
+            this.textBoxWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(0, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(64, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(61, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "height";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(151, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Directory";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(258, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Enabled = true;
+            this.updateTimer.Interval = 500;
+            this.updateTimer.Tick += new System.EventHandler(this.updateTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 401);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDirectory);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "RealTimeImageCutDisplay";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxDirectory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer updateTimer;
+    }
+}
+
---
+++ RealTimeImageCutDisplay/RealTimeImageCutDisplay/Form1.cs
@@ -0,0 +1,191 @@
+﻿using RealTimeImageCutDisplay.Properties;
+using System;
+using System.Data;
+using System.Deployment.Application;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RealTimeImageCutDisplay
+{
+    public partial class Form1 : Form
+    {
+        private string fileName = null;
+        private Point point = new Point();
+        private Size size = new Size();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ScreenSetting();
+
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
+                //**バージョン取得
+                this.Text += " v" + ad.CurrentVersion.ToString();
+                //**最終更新日取得                //update_date = "最終更新日:" + ad.TimeOfLastUpdateCheck.ToLongDateString().ToString();
+            }
+            else
+            {
+                this.Text += " v1.0";
+            }
+            this.Text += " Created By kui";
+
+            textBoxDirectory.Text = Settings.Default.Directory;
+        }
+        private int TextParse(string text)
+        {
+            int result = 0;
+            if(int.TryParse(text, out result) == false){
+                result = 0;
+            }
+            return result;
+        }
+
+        private void timerCheck_Tick(object sender, EventArgs e)
+        {
+            ImageUpdate();
+        }
+        private string GetNewImageFileName()
+        {
+            string fileName = "";
+            string dir = textBoxDirectory.Text;
+            try
+            {
+                if (string.IsNullOrWhiteSpace(dir) == false)
+                {
+                    var files = Directory.GetFiles(dir).OrderBy(f => File.GetLastWriteTime(f));
+                    fileName = files.Last();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return fileName;
+        }
+
+        private void ScreenSetting()
+        {
+            int w = Settings.Default.ClientFormSize.Width;
+            int h = Settings.Default.ClientFormSize.Height;
+            int x = Settings.Default.ClientFormPoint.X;
+            int y = Settings.Default.ClientFormPoint.Y;
+            if (Screen.PrimaryScreen.Bounds.Width < w)
+            {
+                w = Screen.PrimaryScreen.Bounds.Width / 2;
+            }
+
+            if (Screen.PrimaryScreen.Bounds.Height < h)
+            {
+                h = Screen.PrimaryScreen.Bounds.Height / 2;
+            }
+
+            if (x < 0)
+            {
+                x = 0;
+            }
+            if (y < 0)
+            {
+                y = 0;
+            }
+
+            this.Size = new Size(w, h);
+            this.Location = new Point(x, y);
+        }
+        private void ImageUpdate(bool resize = false)
+        {
+            string fileName = GetNewImageFileName();
+            Point point = new Point(TextParse(textBoxX.Text), TextParse(textBoxY.Text));
+            Size size = new Size(TextParse(textBoxWidth.Text), TextParse(textBoxHeight.Text));
+
+            if (string.IsNullOrWhiteSpace(fileName) == false &&
+                (this.fileName != fileName || this.point != point || this.size != size || resize))
+            {
+                this.fileName = fileName;
+                this.point = point;
+                this.size = size;
+
+                try
+                {
+                    //描画先とするImageオブジェクトを作成する
+                    Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    //ImageオブジェクトのGraphicsオブジェクトを作成する
+
+                    using (Graphics g = Graphics.FromImage(canvas))
+                    {
+                        using (Bitmap img = new Bitmap(fileName))
+                        {
+                            //切り取る部分の範囲を決定する。ここでは、位置(10,10)、大きさ100x100
+                            Rectangle srcRect = new Rectangle(point, size);
+                            //描画する部分の範囲を決定する。ここでは、位置(0,0)、大きさ100x100で描画する
+                            Rectangle desRect = new Rectangle(0, 0, pictureBox1.Size.Width, pictureBox1.Size.Height);
+
+                            //補間方法として最近傍補間を指定する
+                            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+                            //画像の一部を描画する
+                            g.DrawImage(img, desRect, srcRect, GraphicsUnit.Pixel);
+
+                            img.Dispose();
+                        }
+                        g.Dispose();
+                    }
+
+                    //PictureBox1に表示する
+                    pictureBox1.Image = canvas;                
+
+                    Settings.Default.Directory = textBoxDirectory.Text;
+                    Settings.Default.ImagePoint = point;
+                    Settings.Default.ImageSize = size;
+                    Settings.Default.Save();
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ImageUpdate();
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            ImageUpdate(true);
+            Settings.Default.ClientFormSize = this.Size;
+            Settings.Default.Save();
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            Settings.Default.ClientFormPoint = this.Location;
+            Settings.Default.Save();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialogクラスのインスタンスを作成
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            //上部に表示する説明テキストを指定する
+            fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            //デフォルトでDesktop
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+
+            //ダイアログを表示する
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                textBoxDirectory.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void updateTimer_Tick(object sender, EventArgs e)
+        {
+            ImageUpdate();
+        }
+    }
+}
